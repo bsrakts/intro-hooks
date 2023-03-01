4d85e5806e72a7fdee322a21de57172f63346e37
Büşra Aktaş <diba.aktas@ogr.sakarya.edu.tr>
2023-03-01 06:35:47 +0300

    create useState exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let number = 10
+  let [data, setData] = useState(100)
+  const setNumber = () => {
+    setData(data + 1)
+  }
+  
+  const showValues = () => {
+    console.log("Number: ", number);
+    console.log("Data: ", data);
+  }
+
+  let sentence ="I learn useState"
+  let [newData, setStringData] = useState("I will practice useState");
+  const setWord = () => {
+    setStringData(newData.replace(
+     "practice", `more practice`
+    ))
+  }
+
+  const showStringValues = () => {
+    console.log("Sentence: ", sentence);
+    console.log("New Data: ", newData);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* number exercise */}
+      <div className='number-exercise' style={{ border: '1px solid rgba(0, 0, 0, 0.05)', lineHeight: 2, padding: 20 }}>
+        Data: {data}
+        <br />
+        Number: {number}
+        <br />
+        <button onClick={() => setNumber()}>Update</button>
+        <br />
+        <button onClick={() => showValues()}>Show Values On Console</button>
+        <br />
+        <button onClick={() => setData("Adolfha")}>Data</button>
+      </div>
+      <br/>
+      {/* string exercise */}
+      <div className='string-exercise' style={{ border: '1px solid rgba(0, 0, 0, 0.05)', lineHeight: 2, padding: 20 }}>
+        String Data: {newData}
+        <br />
+        Sentence: {sentence}
+        <br />
+        <button onClick={() => setWord()}>Update Sentence</button>
+        <br />
+        <button onClick={() => showStringValues()}>Show Values On Console</button>
+        <br />
+        <button onClick={() => setStringData("Adolfha")}>String Data</button>
+      </div>
     </div>
   );
 }
